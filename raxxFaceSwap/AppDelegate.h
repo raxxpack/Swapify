a8b78cfb9d24c0f8f6ff150b1a8788fc176fe2db
Rahim Mitha <rkmitha@gmail.com>
2014-01-29 12:56:42 -0500

    Initial Commit

---
+++ raxxFaceSwap/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  raxxFaceSwap
+//
+//  Created by Rahim Mitha on 1/29/2014.
+//  Copyright (c) 2014 Rahim Mitha. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ raxxFaceSwapTests/raxxFaceSwapTests.m
@@ -0,0 +1,34 @@
+//
+//  raxxFaceSwapTests.m
+//  raxxFaceSwapTests
+//
+//  Created by Rahim Mitha on 1/29/2014.
+//  Copyright (c) 2014 Rahim Mitha. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface raxxFaceSwapTests : XCTestCase
+
+@end
+
+@implementation raxxFaceSwapTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
